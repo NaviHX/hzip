b7a68fde10ca817e47129903357e44f1007d3c2a
NaviHX <932942142@qq.com>
2020-11-06 19:20:37 +0800

    添加输出路径选项

--- src/main.cpp
+++ src/main.cpp
@@ -30,23 +30,35 @@ int main(int argc, char **argv)
                 std::cout << "Usage          : hzip [options] file_path" << std::endl
                           << "-h, --help     : help information" << std::endl
                           << "-c, --compress : compress a file" << std::endl
-                          << "-e, --extract  : extract a file" << std::endl;
+                          << "-e, --extract  : extract a file" << std::endl
+                          << "-o <file_path> : place the output file in <file_path>\n";
             }
             else if (arg == "-c" || arg == "--compress")
                 flag = 0;
             else if (arg == "-e" || arg == "--extract")
                 flag = 1;
+            else if(arg=="-o")
+            {
+                if(i+1<argc)
+                {
+                    string temp(argv[i+1]);
+                    opath=temp;
+                    i++;
+                }
+            }
             else if (arg[0] != '-')
                 path = arg;
         }
         if (flag == 0)
         {
-            opath = path + ".hzp";
+            if(opath=="")
+                opath = path + ".hzp";
             compress(const_cast<char*>(path.c_str()), const_cast<char*>(opath.c_str()));
         }
         else if (flag == 1)
         {
-            opath = path + ".ext";
+            if(opath=="")
+                opath = path + ".ext";
             extract(const_cast<char*>(path.c_str()), const_cast<char*>(opath.c_str()));
         }
         cout << "Done" << endl;
